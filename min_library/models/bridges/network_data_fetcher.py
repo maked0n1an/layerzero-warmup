53d501b3e52afad40e62b89e117eadd1abbe0a9c
maked0n1an <amedik733@gmail.com>
2024-03-22 10:23:38 +0200

    NetworkDataFetcher: updated.

--- min_library/models/bridges/network_data_fetcher.py
+++ min_library/models/bridges/network_data_fetcher.py
@@ -35,12 +35,12 @@ class NetworkDataFetcher:
     @classmethod
     def get_chain_id_and_pool_id(
         cls,
-        network: str,
+        network_name: str,
         token_symbol: str
     ) -> Tuple[int, Optional[int]]:
         token_symbol = token_symbol.upper()
 
-        network_data = cls.get_network_data(network_name=network)
+        network_data = cls.get_network_data(network_name=network_name)
 
         cls._check_for_bridge_contract(
             cls=cls, token=token_symbol, bridge_dict=network_data.bridge_dict
